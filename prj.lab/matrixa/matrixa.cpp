14c7ca60c37fb11dc6dc18f2ce0853ba0474e691
DSafr2506 <122727485+DSafr2506@users.noreply.github.com>
2025-01-17 13:00:44 +0300

    Update matrixa.cpp

--- prj.lab/matrixa/matrixa.cpp
+++ prj.lab/matrixa/matrixa.cpp
@@ -1,60 +1,83 @@
-
-#include <iosfwd>
-#include <cstddef>
-#include <utility>
 #include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <algorithm>
+#include <matrixa/matrixa.hpp>
 
-class MatrixS {
-public:
-    using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
-    using ptrdiff_t = std::ptrdiff_t;  
-public:
-    explicit MatrixS(const SizeType& size = { 0,0 });
-    MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n);
-    MatrixS(const MatrixS& rhs);
-    ~MatrixS();
-
-    MatrixS& operator=(const MatrixS& rhs);
-
-    [[nodiscard]] int& at(const SizeType& elem);
-    [[nodiscard]] const int& at(const SizeType& elem) const;
-    [[nodiscard]] int& at(const std::ptrdiff_t i, const std::ptrdiff_t j);
-    [[nodiscard]] const int& at(const std::ptrdiff_t i, const std::ptrdiff_t j) const;
-    
-	    
-    void resize(const SizeType& new_size);
-    void resize(const std::ptrdiff_t i, const std::ptrdiff_t j);
-
-    [[nodiscard]] const SizeType& ssize() const noexcept;
-    [[nodiscard]] std::ptrdiff_t nRows() const noexcept;
-    [[nodiscard]] std::ptrdiff_t nCols() const noexcept;
-
-private:
-    int** data_{ nullptr };
-    ptrdiff_t m_{ 0 };
-    ptrdiff_t n_{ 0 };
-    SizeType size_{ m_,n_ };
-};
-MatrixS::MatrixS(const SizeType& size):
-    m_(std::get<0>(size)), n_(std::get<1>(size)), data_(nullptr)
-{
-    if (m_<0 || n_<0) {
-        throw std::invalid_argument("Expected positive value");
+MatrixA::MatrixA(std::ptrdiff_t rows, std::ptrdiff_t columns)
+    : n_rows_(rows)
+    , n_cols_(columns) {
+    if (rows <= 0 || columns <= 0) {
+        throw std::invalid_argument("Matrix -> size must be positive");
     }
-    
-    data_ = new int* [m_];
-    for (ptrdiff_t i = 0; i < m_; i += 1) {
-        data_[i] = new int[n_] {};
-    }
-}
-MatrixS::MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n): 
-    MatrixS(std::make_tuple(m, n))
-{
+    data_ = new double[rows * columns] {0.0};
 }
 
-MatrixS::MatrixS(const MatrixS& rhs):
-    m_(rhs.m_), n_(rhs.n_), size_(rhs.size_), data_(nullptr)
-{
-    if (this == &rhs)
-        return;
+
+MatrixA& MatrixA::operator=(const MatrixA& src) {
+    if (this != &src) {
+        double* data = new double[src.n_rows_ * src.n_cols_];
+        std::memcpy(data, src.data_, src.n_rows_ * src.n_cols_ * sizeof(*src.data_));
+        delete[] data_;
+        data_ = data;
+        n_rows_ = src.n_rows_;
+        n_cols_ = src.n_cols_;
+    }
+    return *this;
+}
+
+MatrixA::~MatrixA() {
+    delete[] data_;
+}
+
+MatrixA::MatrixA(const MatrixA& src)
+    : n_rows_(src.n_rows_)
+    , n_cols_(src.n_cols_)
+    , data_(new double[src.n_rows_ * src.n_cols_]) {
+    std::memcpy(data_, src.data_, n_rows_ * n_cols_ * sizeof(*data_));
+}
+
+double& MatrixA::At(const std::ptrdiff_t i_row, const std::ptrdiff_t i_col) {
+    if (i_row < 0 || i_col < 0 || i_row >= n_rows_ || i_col >= n_cols_) {
+        throw std::out_of_range("Matrix -> index out of range");
+    }
+    return data_[i_row * n_cols_ + i_col];
+}
+
+double MatrixA::At(const std::ptrdiff_t i_row, const std::ptrdiff_t i_col) const {
+    if (i_row < 0 || i_col < 0 || i_row >= n_rows_ || i_col >= n_cols_) {
+        throw std::out_of_range("Matrix -> index out of range");
+    }
+    return data_[i_row * n_cols_ + i_col];
+}
+
+// ����� �� ���������� � �������
+void MatrixA::Resize(const std::ptrdiff_t rows, const std::ptrdiff_t cols) {
+    if (rows <= 0 || cols <= 0) {
+        throw std::invalid_argument("Matrix -> sizes must be positive");
+    }
+
+    double* data = new double[rows * cols] {0.0};
+
+    std::ptrdiff_t min_rows = std::min(rows, n_rows_);
+    std::ptrdiff_t min_cols = std::min(cols, n_cols_);
+
+    for (std::ptrdiff_t i = 0; i < min_rows; ++i) {
+        for (std::ptrdiff_t j = 0; j < min_cols; ++j) {
+            data[i * cols + j] = data_[i * n_cols_ + j];
+        }
+    }
+    std::swap(data_, data);
+    delete[] data;
+    n_rows_ = rows;
+    n_cols_ = cols;
+}
+
+void MatrixA::PrintMatrix() const noexcept {
+    for (std::ptrdiff_t i = 0; i < n_rows_; ++i) {
+        for (std::ptrdiff_t j = 0; j < n_cols_; ++j) {
+            std::cout << data_[i * n_cols_ + j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
