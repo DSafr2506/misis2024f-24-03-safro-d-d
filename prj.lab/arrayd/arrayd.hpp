801a6129ed12c55b586d6c178049c44801651200
DSafr2506 <122727485+DSafr2506@users.noreply.github.com>
2025-01-17 12:42:08 +0300

    Update arrayd.hpp

--- prj.lab/arrayd/arrayd.hpp
+++ prj.lab/arrayd/arrayd.hpp
@@ -1,43 +1,43 @@
+// 2024 by Polevoi Dmitry under Unlicense
+
 #pragma once
-#ifndef arrayd_arrayd_HPP_20231303 
-#define arrayd_arrayd_HPP_20231303
+#ifndef ARRAYD_ARRAYD_HPP_20241120
+#define ARRAYD_ARRAYD_HPP_20241120
 
-#include <iosfwd>
 #include <cstddef>
-
 
 class ArrayD {
 public:
-	ArrayD();
-	explicit ArrayD(const std::ptrdiff_t len);
-	ArrayD(const std::ptrdiff_t len, const double num);
-	ArrayD(const ArrayD& rhs);
+	ArrayD() = default;
+
+	ArrayD(const ArrayD&);
+
+	//! \param size - ��������� ������, 0 < size
+	ArrayD(const std::ptrdiff_t size);
+
 	~ArrayD();
 
-	[[nodiscard]]std::ptrdiff_t ssize() const noexcept;
-	void resize(const std::ptrdiff_t new_size);
-	void insert(const std::ptrdiff_t i, const double value);
-	void remove(const std::ptrdiff_t i);
-	void push_back(const double value);
-	void pop_back();
+	ArrayD& operator=(const ArrayD&);
 
-	std::istream& readFrom(std::istream& istrm);
-	std::ostream& writeTo(std::ostream& ostrm) const;
+	[[nodiscard]] std::ptrdiff_t Size() const noexcept { return size_; }
 
-	ArrayD& operator=(const ArrayD& rhs);
-	[[nodiscard]]double& operator[](const std::ptrdiff_t i);
-	[[nodiscard]]const double& operator[](const std::ptrdiff_t i) const;
+	//! \param size - ����� ������, 0 <= size
+	void Resize(const std::ptrdiff_t size);
 
+	//! \param idx - ������  ��������, 0 <= idx < Size()
+	[[nodiscard]] double& operator[](const std::ptrdiff_t idx);
+	[[nodiscard]] double operator[](const std::ptrdiff_t idx) const;
 
+	//! \param idx - ������ ������������ ��������, 0 <= idx <= size 
+	void Insert(const std::ptrdiff_t idx, const double val);
+
+	//! \param idx - ������ ���������� ��������, 0 <= idx < size 
+	void Remove(const std::ptrdiff_t idx);
 
 private:
-	std::ptrdiff_t size_{ 0 };
-	std::ptrdiff_t capacity_{ 0 };
-	double* memory_{ nullptr };
-
-	static const char separator_{ ',' };
+	std::ptrdiff_t capacity_ = 0;  //!< ������ �������
+	std::ptrdiff_t size_ = 0;      //!< ����� ��������� � �������
+	double* data_ = nullptr;             //!< ������
 };
 
-std::istream& operator>>(std::istream& istrm, ArrayD& rhs);
-std::ostream& operator<<(std::ostream& ostrm, const ArrayD& rhs);
-#endif
+#endif // !ARRAYD_ARRAYD_HPP_20241120
